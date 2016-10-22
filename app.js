605eed0de1d42b1a7a5630d246b777474eb49c50
Agustin <agusventura@gmail.com>
2016-10-22 15:40:10 -0300

    Lost

--- app.js
+++ app.js
@@ -31,12 +31,15 @@ app.delete('/jugador/:id',JugadorController.eliminarJugador)
 app.use('/api', jugador);
 
 //mongoose.connect('mongodb://localhost:27017/jugadores', function(err, res) {
-mongoose.connect('mongodb://boludon:boludon@ds063546.mlab.com:63546/heroku_j8bxqbnh', function(err, res) {
+
+mongoose.connect('mongodb://heroku_d7wlc0r5:6veoj0kp4rafpu7lnlagfsqmc@ds015924.mlab.com:15924/heroku_d7wlc0r5', function(err, res) {
   if(err) {
     console.log('ERROR: connecting to Database. ' + err);
   }else{
     console.log("Conectado a la BD.");
   }
+  var mongoURL = process.env.MONGODB_URI;
+  console.log("MongoURL",mongoURL);
   app.listen(80, function() {
     console.log("Boludon WS corriendo en http://localhost:80");
   });
